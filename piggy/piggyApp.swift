d78033851582be3f0ff33787a7ce5ea065231693
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-08-13 14:29:32 +0700

    Initial Commit

---
+++ piggy/piggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  piggyApp.swift
+//  piggy
+//
+//  Created by Dason Tiovino on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct piggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
